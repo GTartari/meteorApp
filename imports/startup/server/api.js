ebf637c1fc0aa35ab6b760b95dcdbf7088ec2606
GTartari <gt_90b@hotmail.com>
2017-01-18 11:34:52 -0300

    reestructura archivos cliente/server

--- imports/startup/server/api.js
+++ imports/startup/server/api.js
@@ -9,8 +9,6 @@ import '../../api/users/server/publications';
 import '../../api/inputs/server/lib/inputObjData.js'
 import '../../api/inputs/server/lib/inputConstrData.js'
 import '../../api/inputs/server/lib/inputOtrosData.js'
-import '../../api/inputs/server/lib/dataUpload.js'
 import '../../api/inputs/server/lib/parseUpload.js'
-import '../../api/inputs/server/lib/dataInfo.js'
 
-import '../../api/inputs/server/publications/dataInfoPost.js'
+import '../../api/inputs/server/publications/dataInfoPost'
